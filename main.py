03f999d98bf9cd46803771ccca473ee983f9d6ca
w40141 <w.40141@gmail.com>
2022-01-16 14:33:30 +0900

    finish query parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -38,9 +39,19 @@ async def read_file(file_path: str):
     return {"file_path": file_path}
 
 
+@app.get("/paras/")
+async def read_paras(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
 
 
 @app.put("/items/{item_id}")
@@ -56,3 +67,13 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
