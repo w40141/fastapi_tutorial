5bd43dc89e144550f8123c3d337eabe09ef238d4
w40141 <w.40141@gmail.com>
2022-01-16 13:43:22 +0900

    add config file

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from enum import Enum
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnt"
+    lenet = "lenet"
 
 
 class Item(BaseModel):
@@ -15,6 +24,20 @@ async def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuls"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
@@ -23,3 +46,13 @@ async def read_item(item_id: int, q: str = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
