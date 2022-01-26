144f5cba9460933a14c57fed61bef2600b2ff164
w40141 <w.40141@gmail.com>
2022-01-26 23:06:59 +0900

    add token and userg

---
+++ api/routers/token.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+import api.schemas.token as token_schema
+from api.core.token import create_access_token
+from api.core.user import authenticate_user
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+router = APIRouter()
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disable": False,
+    }
+}
+
+
+@router.post("/token", response_model=token_schema.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
---
+++ api/routers/user.py
@@ -0,0 +1,17 @@
+from typing import Dict, List, Union
+from fastapi import APIRouter, Depends
+
+import api.schemas.user as user_schema
+from api.core.user import get_current_active_user
+
+router = APIRouter()
+
+
+@router.get("/users/me", response_model=user_schema.User)
+async def read_users_me(current_user: user_schema.User = Depends(get_current_active_user)):
+    return current_user
+
+
+@router.get("/users/me/items", response_model=List[Dict[str, Union[str, user_schema.User]]])
+async def read_own_items(current_user: user_schema.User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
